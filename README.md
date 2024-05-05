eb1bdc83325179056585f05d6d6ccb4cdbcca2a5
Jonathan Gamble <gamblej@gmail.com>
2024-05-05 12:23:34 -0500

    WIP, add various searchable nets

--- README.md
+++ README.md
@@ -1,3 +1,7 @@
 # WTF is this
 
-Just a toolkit to construct chess bots in the browser. You get stockfish classical and lc0. The lc0 can only handle maia (go nodes 1) weights.
+Just a toolkit to construct chess bots in the browser. You get stockfish classical and lc0.
+LC0 uses CPU (OpenBLAS/eigen) so only tiny weights can be used.
+
+For example, go nodes 1 such as https://maiachess.com/ or those with low block/filter sizes like https://github.com/dkappe/leela-chess-weights/wiki/Bad-Gyal
+
--- src/test/node.ts
+++ src/test/node.ts
@@ -11,7 +11,7 @@ const zf = await createZerofish();
 
 console.log('Syntax: <fish|zero> <uci-command> <args>');
 
-zf.setZeroWeights(fs.readFileSync(`${__dirname}/../../wasm/weights.pb`));
+zf.setZeroWeights(fs.readFileSync(`${__dirname}/../../wasm/go-nodes-0.pb`));
 
 readline
   .createInterface({
@@ -20,7 +20,15 @@ readline
     terminal: false,
   })
   .on('line', (line: string) => {
-    if (line.startsWith('fish ')) zf.fish(line.slice(5));
+    if (line === '1') zf.setZeroWeights(fs.readFileSync(`${__dirname}/../../wasm/weights/evilgyal-6.pb`));
+    else if (line === '2')
+      zf.setZeroWeights(fs.readFileSync(`${__dirname}/../../wasm/weights/tinygyal-8.pb`));
+    else if (line === '3')
+      zf.setZeroWeights(fs.readFileSync(`${__dirname}/../../wasm/weights/goodgyal-5.pb`));
+    else if (line === '4') zf.setZeroWeights(fs.readFileSync(`${__dirname}/../../wasm/weights/badgyal-8.pb`));
+    else if (line === '5')
+      zf.setZeroWeights(fs.readFileSync(`${__dirname}/../../wasm/weights/t79-192x15.pb`));
+    else if (line.startsWith('fish ')) zf.fish(line.slice(5));
     else if (line.startsWith('zero ')) zf.zero(line.slice(5));
     if (line === 'zerofish') console.log(zf);
   });
